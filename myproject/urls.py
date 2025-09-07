451685418f42d3bc50c18af0e54d521ea04637d1
Destifaith <josepholaitan419@gmail.com>
2025-09-07 20:18:00 +0000

    Fix: Fully remove drf-yasg, use drf-spectacular only

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,34 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
 from django.views.generic import RedirectView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView  # ← NEW
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="My API",
-      default_version='v1',
-      description="API documentation for deployed Django app",
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('myapp.urls')),
-#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', RedirectView.as_view(url='/swagger/', permanent=False)),  # ← ADD THIS LINE
-     # Swagger UI:
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # ← NEW
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # ← NEW
+    
+    # NEW: Swagger with drf-spectacular
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # Redirect root to Swagger
     path('', RedirectView.as_view(url='/swagger/', permanent=False)),
 ]
